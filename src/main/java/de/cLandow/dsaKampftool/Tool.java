4bd3f79fbd8324ebfda8014772c6136007f1785d
Christoph Landow <clandow@web.de>
2022-08-18 16:01:04 +0200

    feat: added Interface füor ScreenController  nd a charakterScreen Controller

--- src/main/java/de/cLandow/dsaKampftool/Tool.java
+++ src/main/java/de/cLandow/dsaKampftool/Tool.java
@@ -7,16 +7,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Tool extends Application {
 
+    private Character actualCharacter;
     private static Stage primaryStage;
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         primaryStage = stage;
         showSetupScreen();
         primaryStage.show();
+        loadCharacter();
     }
 
     @Override
@@ -24,24 +27,28 @@ public class Tool extends Application {
         cleanup();
     }
 
+    private static void cleanup() {
+    }
+
     private void showSetupScreen() {
         cleanup();
         try {
             // load setup view
-            Parent ingameScreenRoot = FXMLLoader.load(Tool.class.getResource("views/mainSetup.fxml"));
+            Parent ingameScreenRoot = FXMLLoader.load(Objects.requireNonNull(Tool.class.getResource("views/mainSetup.fxml")));
             Scene newestScene = new Scene(ingameScreenRoot);
             // display scene on primary stage
             newestScene.getStylesheets().add("/de/cLandow/dsaKampftool/styles/globalStyles.css");
             primaryStage.setScene(newestScene);
-            //Otherwise, the window was always minimized at TestFX.
+            //Otherwise, the window was always minimized at TestFX later.
             primaryStage.centerOnScreen();
 
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
-    private static void cleanup() {
+    private void loadCharacter() {
+        //TODO: Wenn das charakter erstellen und bearbeiten implementiert wurde muss hier beim öffnen des Tools einer
+        // ausgewählt oder neu erstellt werden
     }
 }
