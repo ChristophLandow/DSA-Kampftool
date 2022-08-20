fb04cd97ca5b0622dd479d78366859855c8d3453
Christoph Landow <clandow@web.de>
2022-08-20 20:18:54 +0200

    feat: added CloseCombatScreenController and changed fxmls

---
+++ src/main/java/de/cLandow/dsaKampftool/controller/CloseCombatScreenController.java
@@ -0,0 +1,25 @@
+package de.cLandow.dsaKampftool.controller;
+
+import javafx.scene.Parent;
+
+public class CloseCombatScreenController implements ScreenController{
+
+    public CloseCombatScreenController(){
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
+}
