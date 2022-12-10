8d79650c5ef73971d2fffacc48fb98107045dccf
Christoph Landow <clandow@web.de>
2022-12-10 13:46:58 +0100

    feat: Character is a normal class now feat: added Ability

--- src/main/java/de/cLandow/dsaKampftool/model/Character.java
+++ src/main/java/de/cLandow/dsaKampftool/model/Character.java
@@ -1,16 +1,66 @@
 package de.cLandow.dsaKampftool.model;
 
-public record Character(
-    //Attribute
-    String name,
-    int at,
-    int pa,
-    int fk,
-    int ini,
+public class Character {
 
-    //Boxes&Buttons
-    boolean kampfreflexe,
-    boolean kampfgespuehr,
-    boolean beengt
-    ){
+    private String name;
+    private int at;
+    private int pa;
+    private int fk;
+    private int ini;
+
+    private boolean kampfreflexe;
+    private boolean kampfgespuehr;
+    private boolean beengt;
+
+    public Character(String name, int at, int pa, int fk, int ini, boolean kampfreflexe, boolean kampfgespuehr, boolean beengt){
+        this.name = name;
+        this.at = at;
+        this.pa = pa;
+        this.fk = fk;
+        this.ini = ini;
+        this.kampfreflexe = kampfreflexe;
+        this.kampfgespuehr = kampfgespuehr;
+        this.beengt = beengt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAt() {
+        return at;
+    }
+
+    public int getPa() {
+        return pa;
+    }
+
+    public int getFk() {
+        return fk;
+    }
+
+    public int getIni() {
+        return ini;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAt(int at) {
+        this.at = at;
+    }
+
+    public void setPa(int pa) {
+        this.pa = pa;
+    }
+
+    public void setFk(int fk) {
+        this.fk = fk;
+    }
+
+    public void setIni(int ini) {
+        this.ini = ini;
+    }
+
 }
