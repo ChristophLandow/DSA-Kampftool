146fd84ddc1c417299e463abf96be1053631f497
Christoph Landow <clandow@web.de>
2023-06-10 12:12:36 +0200

    feat: added addSpecialAbility.fxml and implemented Methods to open and close

--- src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/SpecialAbilityPopupController.java
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/SpecialAbilityPopupController.java
@@ -1,8 +1,10 @@
 package de.cLandow.dsaKampftool.controller.subcontroller;
 
 import de.cLandow.dsaKampftool.Tool;
+import de.cLandow.dsaKampftool.controller.CharacterScreenController;
 import de.cLandow.dsaKampftool.controller.ScreenController;
 import de.cLandow.dsaKampftool.model.Ability;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,21 +15,17 @@ import java.util.ArrayList;
 
 public class SpecialAbilityPopupController implements ScreenController {
 
-    //TODO: Daas Hinzufügen neuer Sonderfertigkeiten ist so nervig, am besten Popup Fenster machen mit dem man aus einer
-    // Liste alle auswählen kann und gut
-
-    @FXML ChoiceBox<String> specialAbilityChoiceBox;
+    private final CharacterScreenController characterScreenController;
 
     private final ArrayList<Ability> specialAbilityList = new ArrayList<>();
 
-    public  SpecialAbilityPopupController(){
-
+    public  SpecialAbilityPopupController(CharacterScreenController characterScreenController){
+        this.characterScreenController = characterScreenController;
     }
 
     @Override
     public void init() {
         loadSpecialAbilityList();
-        loadChoiceBox();
     }
 
     @Override
@@ -38,7 +36,7 @@ public class SpecialAbilityPopupController implements ScreenController {
     @Override
     public Parent render() {
         Parent parent = null;
-        final FXMLLoader loader = new FXMLLoader(Tool.class.getResource("views/subViews/SpecialAbilityBox.fxml"));
+        final FXMLLoader loader = new FXMLLoader(Tool.class.getResource("views/subViews/addAbilityPopup.fxml"));
         loader.setControllerFactory(c -> this);
         try {
             parent = loader.load();
@@ -48,14 +46,15 @@ public class SpecialAbilityPopupController implements ScreenController {
         return parent;
     }
 
-    private void loadChoiceBox() {
-        for (Ability ability : specialAbilityList){
-            specialAbilityChoiceBox.getItems().add(ability.abilityName());
-        }
-    }
-
     private void loadSpecialAbilityList() {
         specialAbilityList.add(new Ability("Kampfreflexe",0,0,4,
                 ""));
+    }
+
+    public void closeAddSpecialAbilityPopup(ActionEvent actionEvent) {
+        characterScreenController.closeAddSpecialAbilityPopup();
+    }
+
+    public void addSpecialAbilityToHero(ActionEvent actionEvent) {
     }
 }
