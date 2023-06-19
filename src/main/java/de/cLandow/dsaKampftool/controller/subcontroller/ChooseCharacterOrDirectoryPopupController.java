63d20b254a16e0ec6b8fd92a84b2207a31d2a08a
Christoph Landow <clandow@web.de>
2023-06-19 10:50:41 +0200

    feat: added ChooseCharacterOrDirectoryPopupController and chooseCharacterOrSetDirectoryPopup.fxml

---
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/ChooseCharacterOrDirectoryPopupController.java
@@ -0,0 +1,25 @@
+package de.cLandow.dsaKampftool.controller.subcontroller;
+
+import de.cLandow.dsaKampftool.controller.RenderController;
+import javafx.scene.Parent;
+
+public class ChooseCharacterOrDirectoryPopupController implements RenderController {
+
+    public ChooseCharacterOrDirectoryPopupController(){
+
+    }
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
+}
