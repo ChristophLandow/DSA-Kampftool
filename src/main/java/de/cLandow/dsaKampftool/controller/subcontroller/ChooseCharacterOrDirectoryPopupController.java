d110983b27f81d1e0dff6890ee2124b564ed001f
Christoph Landow <clandow@web.de>
2023-06-19 10:54:49 +0200

    feat: added main-Methods to ChooseCharacterOrDirectoryPopupController

--- src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/ChooseCharacterOrDirectoryPopupController.java
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/ChooseCharacterOrDirectoryPopupController.java
@@ -1,12 +1,16 @@
 package de.cLandow.dsaKampftool.controller.subcontroller;
 
 import de.cLandow.dsaKampftool.controller.RenderController;
+import de.cLandow.dsaKampftool.controller.SetupScreenController;
+import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 
 public class ChooseCharacterOrDirectoryPopupController implements RenderController {
 
-    public ChooseCharacterOrDirectoryPopupController(){
+    private SetupScreenController setupScreenController;
 
+    public ChooseCharacterOrDirectoryPopupController(SetupScreenController setupScreenController){
+        this.setupScreenController = setupScreenController;
     }
     @Override
     public void init() {
@@ -21,5 +25,16 @@ public class ChooseCharacterOrDirectoryPopupController implements RenderControll
     @Override
     public Parent render() {
         return null;
+    }
+
+    public void chooseDirectory(ActionEvent actionEvent) {
+    }
+
+    public void exitTool(ActionEvent actionEvent) {
+        stop();
+
+    }
+
+    public void openCreateOrChooseCharacterPopup(ActionEvent actionEvent) {
     }
 }
