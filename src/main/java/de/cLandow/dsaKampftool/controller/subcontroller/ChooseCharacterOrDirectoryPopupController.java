0b0c8ea7c26497bab3f94a8eab71f028581e1803
Christoph Landow <clandow@web.de>
2023-06-26 09:23:30 +0200

    fix: deleted fdirectory in HomeDirectory

--- src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/ChooseCharacterOrDirectoryPopupController.java
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/ChooseCharacterOrDirectoryPopupController.java
@@ -14,6 +14,7 @@ import javafx.stage.DirectoryChooser;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 public class ChooseCharacterOrDirectoryPopupController implements RenderController {
 
