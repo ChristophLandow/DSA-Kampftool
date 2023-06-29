2d0656deb4ca02ff34e2b7d9225ebda163f48612
Christoph Landow <clandow@web.de>
2023-06-29 12:52:56 +0200

    feat: deleted unused fields in model records

--- src/main/java/de/cLandow/dsaKampftool/model/Weapon_closeCombat.java
+++ src/main/java/de/cLandow/dsaKampftool/model/Weapon_closeCombat.java
@@ -1,10 +1,7 @@
 package de.cLandow.dsaKampftool.model;
 
-public record Weapon_closeCombat(
+public record Weapon_closeCombat (
         String name,
-        String tp,
-        String tpkk,
-        int bf,
         int ini,
         String wm,
         String dk
