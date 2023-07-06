015d653bb18820b8b2cff29c9e700fdfd16973b4
Christoph Landow <clandow@web.de>
2023-07-06 10:16:16 +0200

    feat: added GearListItemController and connected it with gearListItem.fxml

---
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/GearListItemController.java
@@ -0,0 +1,26 @@
+package de.cLandow.dsaKampftool.controller.subcontroller;
+
+import de.cLandow.dsaKampftool.controller.RenderController;
+import javafx.scene.Parent;
+
+public class GearListItemController implements RenderController {
+
+    public GearListItemController(){
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
+}
