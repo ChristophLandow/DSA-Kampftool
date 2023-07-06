25d9d6cb689e003b48fc56baf31296ee5608bea1
Christoph Landow <clandow@web.de>
2023-07-06 10:17:20 +0200

    feat: added FXML Elements in GearListItemController

--- src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/GearListItemController.java
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/GearListItemController.java
@@ -1,9 +1,15 @@
 package de.cLandow.dsaKampftool.controller.subcontroller;
 
 import de.cLandow.dsaKampftool.controller.RenderController;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class GearListItemController implements RenderController {
+
+    @FXML Label ItemNameLabel;
+    @FXML AnchorPane gearListItemAnchor;
 
     public GearListItemController(){
 
