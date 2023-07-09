328667e68be4ba12abd02dd68935ed6f3fe0db3c
Christoph Landow <clandow@web.de>
2023-07-09 17:38:17 +0200

    feat: added EquipmentController

---
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/EquipmentController.java
@@ -0,0 +1,25 @@
+package de.cLandow.dsaKampftool.controller.subcontroller;
+
+import de.cLandow.dsaKampftool.controller.RenderController;
+import javafx.scene.Parent;
+
+public class EquipmentController implements RenderController {
+
+    public EquipmentController(){
+
+    }
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
+}
