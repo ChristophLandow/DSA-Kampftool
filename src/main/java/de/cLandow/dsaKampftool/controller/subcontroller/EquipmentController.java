b736b3b2e8de1ffba7f07a582449e416f3746456
Christoph Landow <clandow@web.de>
2023-07-10 11:02:28 +0200

    feat: added new FXML Elements to EquipmentController and SelectedGearBoxController

--- src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/EquipmentController.java
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/EquipmentController.java
@@ -1,15 +1,30 @@
 package de.cLandow.dsaKampftool.controller.subcontroller;
 
 import de.cLandow.dsaKampftool.controller.RenderController;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import static de.cLandow.dsaKampftool.Constants.MAINWEAPON;
+import static de.cLandow.dsaKampftool.Constants.MAINWEAPON_IMAGE;
 
 public class EquipmentController implements RenderController {
+
+    @FXML BorderPane equipmentContainer;
+    @FXML ImageView equipmentImageView;
 
     public EquipmentController(){
 
     }
     @Override
     public void init() {
+        addHoverEffekt();
 
     }
 
@@ -22,4 +37,39 @@ public class EquipmentController implements RenderController {
     public Parent render() {
         return null;
     }
+
+    private void addHoverEffekt() {
+        //addBorderPaneEffect((ImageView) pane.getCenter());
+        //addToolTipp((ImageView) pane.getCenter());
+    }
+
+    private void addBorderPaneEffect(ImageView view) {
+        view.setOnMouseEntered((event) -> {
+
+        });
+    }
+    private void addToolTipp(ImageView view) {
+        Tooltip newTooltip = new Tooltip("test");
+        Tooltip.install(view,newTooltip);
+    }
+
+    private void changeIconToSetMode(String item) {
+        switch(item){
+            case MAINWEAPON -> {
+                equipmentImageView.setImage(loadImage(MAINWEAPON_IMAGE));
+            }
+        }
+    }
+
+    private Image loadImage(String path){
+        FileInputStream input = null;
+        try {
+            input = new FileInputStream(path);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return new Image(input);
+    }
+
+
 }
--- src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/SelectedGearBoxController.java
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/SelectedGearBoxController.java
@@ -14,6 +14,7 @@ import javafx.scene.Parent;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -22,29 +23,20 @@ import java.io.IOException;
 public class SelectedGearBoxController implements RenderController {
 
 
-    @FXML BorderPane shieldBorderPane;
-    @FXML BorderPane mainWeaponBorderPane;
-    @FXML BorderPane sideWeaponBorderPane;
-    @FXML BorderPane headBorderPane;
-    @FXML BorderPane torsoBorderPAne;
-    @FXML BorderPane leftArmBorderPane;
-    @FXML BorderPane tummyBorderPane;
-    @FXML BorderPane leftLegBorderPane;
-    @FXML BorderPane rightLegBorderPane;
-    @FXML BorderPane rightArmBorderPane;
+    public HBox mainWeaponHBox;
 
-    @FXML ImageView rightLegImageView;
-    @FXML ImageView shieldImageView;
-    @FXML ImageView sideWeaponImageView;
-    @FXML ImageView mainWeaponImageView;
-    @FXML ImageView helmetImageView;
-    @FXML ImageView torsoArmorImageView;
-    @FXML ImageView tummyImageView;
-    @FXML ImageView leftLegImageView;
-    @FXML ImageView leftArmImageView;
-    @FXML ImageView rightArmImageView;
+
 
     private final AddGearPopupController addGearPopupController;
+    @FXML HBox sideWeaponHBox;
+    @FXML HBox shieldHBox;
+    @FXML HBox helmetHBox;
+    @FXML HBox torsoHBox;
+    @FXML HBox rightArmHBox;
+    @FXML HBox leftArmHBox;
+    @FXML HBox tummyHBox;
+    @FXML HBox rightLegHBox;
+    @FXML HBox leftLegHBox;
 
     private Weapon_closeCombat mainWeapon;
     private Weapon_closeCombat sideWeapon;
@@ -57,16 +49,7 @@ public class SelectedGearBoxController implements RenderController {
 
     @Override
     public void init() {
-        addHoverEffekt(headBorderPane);
-        addHoverEffekt(torsoBorderPAne);
-        addHoverEffekt(tummyBorderPane);
-        addHoverEffekt(leftLegBorderPane);
-        addHoverEffekt(rightLegBorderPane);
-        addHoverEffekt(leftArmBorderPane);
-        addHoverEffekt(rightArmBorderPane);
-        addHoverEffekt(shieldBorderPane);
-        addHoverEffekt(mainWeaponBorderPane);
-        addHoverEffekt(sideWeaponBorderPane);
+
     }
 
     @Override
@@ -86,43 +69,9 @@ public class SelectedGearBoxController implements RenderController {
         return parent;
     }
 
-    private void addHoverEffekt(BorderPane pane) {
-        //addBorderPaneEffect((ImageView) pane.getCenter());
-        addToolTipp((ImageView) pane.getCenter());
-    }
-
-    private void addBorderPaneEffect(ImageView view) {
-        view.setOnMouseEntered((event) -> {
-
-        });
-    }
-
-    private void addToolTipp(ImageView view) {
-        Tooltip newTooltip = new Tooltip("test");
-        Tooltip.install(view,newTooltip);
-    }
-
     public void setMainWeapon(Weapon_closeCombat mainWeapon) {
         this.mainWeapon = mainWeapon;
-        changeIconToSetMode(MAINWEAPON);
-    }
-
-    private void changeIconToSetMode(String item) {
-        switch(item){
-            case MAINWEAPON -> {
-                mainWeaponImageView.setImage(loadImage(MAINWEAPON_IMAGE));
-            }
-        }
-    }
-
-    private Image loadImage(String path){
-        FileInputStream input = null;
-        try {
-            input = new FileInputStream(path);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        return new Image(input);
+        //changeIconToSetMode(MAINWEAPON);
     }
 
     public Weapon_closeCombat getMainWeapon(){
