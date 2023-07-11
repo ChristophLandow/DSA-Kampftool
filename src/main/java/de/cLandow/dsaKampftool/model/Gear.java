404fec60ad84d79674ff66d1711be314d88cd6b4
Christoph Landow <clandow@web.de>
2023-07-11 12:16:41 +0200

    feat: Gear Class Attribute Name

--- src/main/java/de/cLandow/dsaKampftool/model/Gear.java
+++ src/main/java/de/cLandow/dsaKampftool/model/Gear.java
@@ -1,4 +1,17 @@
 package de.cLandow.dsaKampftool.model;
 
 public class Gear {
+
+    private String name;
+    public Gear(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
