fbbd94cbbab8fddd61030dce266f988715ecf241
Christoph Landow <clandow@web.de>
2023-07-19 23:38:48 +0200

    chore: added Ability "Ausweichen I" in WriteAbilityFileService

--- src/main/java/de/cLandow/dsaKampftool/services/WriteAbilityFileService.java
+++ src/main/java/de/cLandow/dsaKampftool/services/WriteAbilityFileService.java
@@ -60,6 +60,14 @@ public class WriteAbilityFileService {
             writer.writeAttribute(ENCUMBRANCE_MOD, "0");
             writer.writeEndElement();
 
+            writer.writeStartElement(ABILITY);
+            writer.writeAttribute(NAME, "Ausweichen I");
+            writer.writeAttribute(ATTACK_MOD, "0");
+            writer.writeAttribute(PARADE_MOD, "3");
+            writer.writeAttribute(INITIATIVEMOD, "0");
+            writer.writeAttribute(ENCUMBRANCE_MOD, "0");
+            writer.writeEndElement();
+
             writer.writeEndElement();
 
             writer.writeEndDocument();
