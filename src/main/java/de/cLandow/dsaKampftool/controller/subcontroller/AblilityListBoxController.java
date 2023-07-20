6cda76c150cdd95873be05778408f405a394a2e7
Christoph Landow <clandow@web.de>
2023-07-20 09:23:41 +0200

    feat: added AbilityListBoxController

---
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/AblilityListBoxController.java
@@ -0,0 +1,26 @@
+package de.cLandow.dsaKampftool.controller.subcontroller;
+
+import de.cLandow.dsaKampftool.controller.RenderController;
+import javafx.scene.Parent;
+
+public class AblilityListBoxController implements RenderController {
+
+    public AblilityListBoxController(){
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
+}
