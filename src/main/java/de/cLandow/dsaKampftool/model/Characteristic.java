e0f86e1548893c7fed101b30a93832baaff17995
Christoph Landow <clandow@web.de>
2023-07-28 09:10:30 +0200

    feat: added Characteristic in models

---
+++ src/main/java/de/cLandow/dsaKampftool/model/Characteristic.java
@@ -0,0 +1,41 @@
+package de.cLandow.dsaKampftool.model;
+
+import javafx.collections.ObservableList;
+
+public class Characteristic {
+
+    private final String name;
+    private final Integer atkMod;
+    private final Integer defMod;
+    private final Integer initMod;
+    private final ObservableList<Ability> abilityList;
+
+    public Characteristic(String name, Integer atkMod, Integer defMod, Integer initMod, ObservableList<Ability> abilityList){
+        this.name = name;
+        this.atkMod = atkMod;
+        this.defMod = defMod;
+        this.initMod = initMod;
+        this.abilityList = abilityList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAtkMod() {
+        return atkMod;
+    }
+
+    public Integer getDefMod() {
+        return defMod;
+    }
+
+    public Integer getInitMod() {
+        return initMod;
+    }
+
+    public ObservableList<Ability> getAbilityList() {
+        return abilityList;
+    }
+
+}
