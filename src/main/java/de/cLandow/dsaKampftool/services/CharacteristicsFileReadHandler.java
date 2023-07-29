28a425aa2a661e275f09c141e277f73fd3be7ff4
Christoph Landow <clandow@web.de>
2023-07-29 11:13:44 +0200

    feat: override methods in CharacteristicsFileReadHandler

--- src/main/java/de/cLandow/dsaKampftool/services/CharacteristicsFileReadHandler.java
+++ src/main/java/de/cLandow/dsaKampftool/services/CharacteristicsFileReadHandler.java
@@ -2,9 +2,37 @@ package de.cLandow.dsaKampftool.services;
 
 import de.cLandow.dsaKampftool.model.Characteristic;
 import javafx.collections.ObservableList;
+import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class CharacteristicsFileReadHandler extends DefaultHandler {
+
+    @Override
+    public void startDocument(){
+    }
+
+    @Override
+    public void endDocument(){
+
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
+
+
+    }
+
+    //Heißt endElement, wird aber noch vor startElement aufgerufen
+    @Override
+    public void endElement(String uri, String localName, String qName){
+
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length){
+
+    }
+
     public ObservableList<Characteristic> getObservableList() {
         return null;
     }
