091547526b1c11695de531b8013452fc83432f64
Christoph Landow <clandow@web.de>
2023-07-30 14:53:15 +0200

    chore: added group to constructors of Advantage and Drawback

--- src/main/java/de/cLandow/dsaKampftool/model/Advantage.java
+++ src/main/java/de/cLandow/dsaKampftool/model/Advantage.java
@@ -3,7 +3,7 @@ package de.cLandow.dsaKampftool.model;
 import javafx.collections.ObservableList;
 
 public class Advantage extends Characteristic{
-    public Advantage(String name, Integer atkMod, Integer defMod, Integer initMod, ObservableList<Ability> abilityList) {
-        super(name, atkMod, defMod, initMod, abilityList);
+    public Advantage(String name, String group, Integer atkMod, Integer defMod, Integer initMod, ObservableList<Ability> abilityList) {
+        super(name, group,  atkMod, defMod, initMod, abilityList);
     }
 }
