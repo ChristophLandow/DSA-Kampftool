72a469a26805bb4116ed3b001782a6164f57cbdd
Christoph Landow <clandow@web.de>
2023-08-01 07:47:38 +0200

    fix: listener remove when stop SelectedCharacteristicsBoxController

--- src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/SelectedCharacteristicsBoxController.java
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/SelectedCharacteristicsBoxController.java
@@ -34,7 +34,7 @@ public class SelectedCharacteristicsBoxController implements RenderController {
 
     @Override
     public void stop() {
-
+        characteristicObservableList.removeListener(listChangeListener);
     }
 
     @Override
