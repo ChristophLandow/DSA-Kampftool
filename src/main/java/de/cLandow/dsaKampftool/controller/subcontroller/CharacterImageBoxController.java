707e765fb1684c322e180ecc8f5ab4695bea3cb7
Christoph Landow <clandow@web.de>
2023-08-04 23:07:52 +0200

    feat: added CharacterImageBoxController

---
+++ src/main/java/de/cLandow/dsaKampftool/controller/subcontroller/CharacterImageBoxController.java
@@ -0,0 +1,26 @@
+package de.cLandow.dsaKampftool.controller.subcontroller;
+
+import de.cLandow.dsaKampftool.controller.RenderController;
+import javafx.scene.Parent;
+
+public class CharacterImageBoxController implements RenderController {
+
+    public CharacterImageBoxController(){
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
+}
