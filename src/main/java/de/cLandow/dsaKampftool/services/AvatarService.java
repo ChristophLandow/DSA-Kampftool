8584020bb421579472fd0115b6e850ebfbf61dfc
Christoph Landow <clandow@web.de>
2023-08-14 11:22:20 +0200

    fix: correct filepath for STANDARD_CHARACTER_AVATAR_FILEPATH

--- src/main/java/de/cLandow/dsaKampftool/services/AvatarService.java
+++ src/main/java/de/cLandow/dsaKampftool/services/AvatarService.java
@@ -8,6 +8,7 @@ import javafx.stage.FileChooser;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
